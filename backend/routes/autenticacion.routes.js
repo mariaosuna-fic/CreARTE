8a42188b8320ec91972dec98b8e5e808b5e3f36a
Marco Quintero <marcoquintero33@gmail.com>
2026-05-14 13:43:24 -0700

    implementacion de autenticacion segura con bcrypt (registro y login)

--- backend/routes/autenticacion.routes.js
+++ backend/routes/autenticacion.routes.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const conexion = require('../database/db');
+const bcrypt = require('bcrypt');
 
 // ======================
 // REGISTRO
 // ======================
 
-router.post('/registro', (req, res) => {
+router.post('/registro', async (req, res) => {
 
     const { nombre, correo, contraseña, rol } = req.body;
+    const contraseñaHash = await bcrypt.hash(contraseña, 10);
 
     const sql = `
         INSERT INTO Usuario(nombre, correo, contraseña, rol, estado)
@@ -18,7 +20,7 @@ router.post('/registro', (req, res) => {
 
     conexion.query(
         sql,
-        [nombre, correo, contraseña, rol],
+        [nombre, correo, contraseñaHash, rol],
         (error, resultado) => {
 
             if (error) {
@@ -45,19 +47,19 @@ router.post('/registro', (req, res) => {
 // LOGIN
 // ======================
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
 
     const { correo, contraseña } = req.body;
 
     const sql = `
         SELECT * FROM Usuario
-        WHERE correo = ? AND contraseña = ?
+        WHERE correo = ?
     `;
 
     conexion.query(
         sql,
-        [correo, contraseña],
-        (error, resultados) => {
+        [correo],
+        async (error, resultados) => {
 
             if (error) {
 
@@ -76,10 +78,24 @@ router.post('/login', (req, res) => {
                 });
 
             }
+            const usuario = resultados[0];
+
+            const contraseñaValida = await bcrypt.compare(
+                contraseña,
+                usuario.contraseña
+            );
+
+            if (!contraseñaValida) {
+
+                return res.status(401).json({
+                mensaje: 'Correo o contraseña incorrectos'
+                });
+
+            }
 
             res.json({
                 mensaje: 'Inicio de sesión exitoso',
-                usuario: resultados[0]
+                usuario: usuario
             });
 
         }
