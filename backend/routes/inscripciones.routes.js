c4851eaa08bfc31862364cd162467a915ed7095f
MariaOsunaM <maria.osuna@gmail.com>
2026-05-20 20:45:54 -0700

    Arreglo ruta a la base de datos

--- backend/routes/inscripciones.routes.js
+++ backend/routes/inscripciones.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const db = require('../config/db');
+const db = require('../database/db');
 
 const verificarToken =
     require('../middlewares/verificarToken');
